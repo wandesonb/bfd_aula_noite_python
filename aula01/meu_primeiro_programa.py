7f19521da292a08dfc59233843d32fe8f4ace36e
wandesonb <twandeson13@gmail.com>
2025-08-08 00:02:30 +0000

    aula com exercicios

--- aula01/meu_primeiro_programa.py
+++ aula01/meu_primeiro_programa.py
@@ -1,2 +1,21 @@
-print("ola mundo!!", end="\n")
-print("ola mundo!!"+"eu sou Wandeson")
+print("bom dia!")
+print("sextou!")
+print("desligue o celular!")
+
+print("desligue\n o celular")
+
+print("bom dia!", "sextou!", "desligue o celular")
+print("bom dia!" + "sextou!" + "desligue o celular!")
+
+print("Nome: João\n")
+print("Idade: 20 anos\n")
+print("Curso: Python Básico")
+
+print("Nome: João\n"+"Idade: 20 anos\n"+"Curso: Python Básico")
+
+
+nome="Wandeson"
+idade=34
+curso="python básico"
+ 
+print("minha idade é"
